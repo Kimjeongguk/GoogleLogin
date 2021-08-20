21975a0a130db1e2b62656b3b979810d9fff28ba
KimJeongguk <mindstar7949@naver.com>
2021-08-20 23:08:57 +0900

    userInfo

--- GoogleLoginEx/AppDelegate.swift
+++ GoogleLoginEx/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GIDSignIn.sharedInstance()?.clientID = "330599781263-7rnge85asous8jrt3a45dji96368hh2l.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance()?.delegate = self
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return (GIDSignIn.sharedInstance()?.handle(url))!
     }
 
     // MARK: UISceneSession Lifecycle
@@ -31,6 +38,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+}
 
+extension AppDelegate: GIDSignInDelegate {
+    public static var user: GIDGoogleUser!
+        
+        func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+            if let error = error {
+                if(error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                    print("not signed in before or signed out")
+                } else {
+                    print(error.localizedDescription)
+                }
+            }
+            
+            // singleton 객체 - user가 로그인을 하면, AppDelegate.user로 다른곳에서 사용 가능
+            AppDelegate.user = user
+            
+            return
+        }
 }
 
--- GoogleLoginEx/ViewController.swift
+++ GoogleLoginEx/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    var loginBtn = GIDSignInButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance()?.presentingViewController = self // 로그인화면 불러오기
+            GIDSignIn.sharedInstance()?.restorePreviousSignIn() // 자동로그인
+
     }
 
-
+    @IBAction func login(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signIn()
+    }
+    
+    @IBAction func userInfo(_ sender: Any) {
+        let user = AppDelegate.user
+            
+            self.idLabel.text = user?.userID
+        print(user?.userID)
+            self.emailLabel.text = user?.profile.email
+        print(user?.profile.email)
+            self.nameLabel.text = user?.profile.name
+        print(user?.profile.name)
+    }
 }
 
